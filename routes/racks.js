38893f423d3ff661341306a1898bb89b86a9ff5b
SaralynOgden <Saralyn.Ogden@gmail.com>
2017-01-03 13:01:31 -0800

    basic server and route

---
+++ routes/racks.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router(); // eslint-disable-line new-cap
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/bike_data', (err, db) => {
+  router.get('/racks/:lat/:lng', (req, res, next) => {
+    const { lat, lng } = req.params;
+    db.collection('racks').find({
+      location: {
+        $near: {
+          $geometry: { type: "Point",
+                       coordinates: [parseFloat(lng), parseFloat(lat)] },
+          $maxDistance: 770
+        }
+      }
+    }).limit(20).toArray((err, racks) => {
+      if (err) throw err;
+      Promise.all(racks.map((rack) => {
+        return db.collection('thefts').find({
+          location: {
+            $near: {
+              $geometry: { type: "Point",
+                           coordinates: rack.location.coordinates },
+              $maxDistance: 100
+            }
+          }
+        }).toArray();
+      })).then((thefts) => {
+        for (let i = 0; i < racks.length; i++) {
+          racks[i]['thefts'] = thefts[i];
+        }
+        res.send(racks);
+      })
+    });
+  });
+
+  router.post('/racks', (req, res, next) => {
+    const { address, color, location, rackCapacity } = req.body;
+
+    db.collection('racks')
+        .insertOne({ address, color, location,
+                     rackCapacity, inputFrom: 'user' }, (result) => console.log(result));
+  });
+})
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+
+app.disable('x-powered-by');
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+const path = require('path');
+
+app.use(express.static(path.join('public')));
+
+const racks = require('./routes/racks.js');
+
+app.use(racks);
+
+app.use((_req, res) => {
+  res.sendStatus(404);
+});
+
+// eslint-disable-next-line max-params
+app.use((err, _req, res, _next) => {
+  if (err.output && err.output.statusCode) {
+    return res
+      .status(err.output.statusCode)
+      .set('Content-Type', 'text/plain')
+      .send(err.message);
+  }
+
+  // eslint-disable-next-line no-console
+  console.error(err.stack);
+  res.sendStatus(500);
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  if (app.get('env') !== 'test') {
+    // eslint-disable-next-line no-console
+    console.log('Listening on port', port);
+  }
+});
+
+module.exports = app;
